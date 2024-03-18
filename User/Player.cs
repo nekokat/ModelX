8bf9260770fa62989bf7f5cfccaa4032e0f04b04
Максим Халяпин <nekokat89@gmail.com>
2024-03-18 12:58:33 +0300

    update Player constructor

--- User/Player.cs
+++ User/Player.cs
@@ -1,23 +1,31 @@
-﻿using System.Diagnostics;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace User{
+    
+    [JsonObject(MemberSerialization.OptIn)]
     public class Player
     {
-
-        public Player(string firstName, string lastName, int age)
+        public Player(string firstName, string lastName, int age, string? email, string? phone)
         {
             FirstName = firstName;
             LastName = lastName;
             Age = age;
-            Email = null;
-            Phone = null;
+            Email = email;
+            Phone = phone;
         }
+        
+        public Player(string firstName, string lastName, int age) : this(firstName, lastName, age, null, null){}
 
+        [JsonProperty]
         public string? FirstName{ get; set;}
+        [JsonProperty]
         public string? LastName{ get; set;}
+        [JsonProperty]
         public int? Age { get; set;}
-
+        [JsonProperty]
         public string? Email {get; set;}
+        [JsonProperty]
         public string? Phone {get; set;}
 
         public override string ToString()
