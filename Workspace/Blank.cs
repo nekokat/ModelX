e78667653866e91f458aa22763b0790d85c8ca4a
Maxim Khalyapin <nekokat89@gmail.com>
2024-03-30 19:37:33 +0300

    update

--- Workspace/Blank.cs
+++ Workspace/Blank.cs
@@ -13,32 +13,33 @@ namespace ModelX.Workspace.Blank
         public BlankType? Type { get; set; }
 
         [JsonProperty]
-        public int? Version { get; set; }
+        public Support.Version? Version { get; set; } = new();
 
         [JsonProperty]
         public ID? Id { get; set; }
 
+        /*
         [JsonProperty]
         public int? TotalPoints { get; set; }
 
-        /*
+        
         [JsonProperty]
         public List<PointsRecord>? points_record { get; set; }
-        */
+        
         [JsonProperty]
         public Profile? profile { get; set; }
-        /*
+        
         [JsonProperty]
         public Settings? settings { get; set; }
-        */
+        
 
         [JsonProperty]
         public List<Attribute>? Attributes { get; set; }
 
-        /*
+        
         [JsonProperty]
         public List<Trait>? traits { get; set; }
-        */
+        
 
         [JsonProperty]
         public DateTime CreatedDate { get; set; }
@@ -46,7 +47,7 @@ namespace ModelX.Workspace.Blank
         [JsonProperty]
         public DateTime ModifiedDate { get; set; }
 
-        /*
+        
         [JsonProperty]
         public Calc? calc { get; set; }
         */
--- Workspace/Create.Blank/Create.cs
+++ Workspace/Create.Blank/Create.cs
@@ -1,18 +1,45 @@
-﻿using ModelX.Workspace.Blank
+﻿using ModelX.Workspace.Blank;
+using Support;
+using Newtonsoft.Json;
 
-namespace ModelX.Workspace.Create.Blank
+namespace ModelX.Workspace.CreateBlank
 {
 
     public class Create
     {
+        public static void GenerateEmptyJson()
+        {
+            foreach (BlankType type in (BlankType[]) Enum.GetValues(typeof(BlankType)))
+            {
+                using (StreamWriter file = File.CreateText(@$".\Template\{Enum.GetName(typeof(BlankType), type)}.json"))
+                {
+                    Blank.Blank _data = new ();
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(file, _data);
+                }
+            }
+        }
+
+        public Create() : this(BlankType.Clear) {}
+
         public Create(BlankType type)
         {
             Type = type;
-            
+            Version = new(0,0,1);
         }
+
+        public Create(BlankType type, Support.Version version)
+        {
+            Type = type;
+            Version = version;
+        }
+
+        Support.Version Version {get; set; }
 
         BlankType Type { get; set; }
 
-        public Create() : this(BlankType.Clear) {}
+
+
+
     }
 }
