39a98850a57e78af358b253ad5ac0e716955734f
Maxim Khalyapin <nekokat89@gmail.com>
2024-03-30 17:25:43 +0300

    Add Create Blank

---
+++ Workspace/Create.Blank/Create.cs
@@ -0,0 +1,18 @@
+﻿using ModelX.Workspace.Blank
+
+namespace ModelX.Workspace.Create.Blank
+{
+
+    public class Create
+    {
+        public Create(BlankType type)
+        {
+            Type = type;
+            
+        }
+
+        BlankType Type { get; set; }
+
+        public Create() : this(BlankType.Clear) {}
+    }
+}
