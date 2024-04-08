0204a7ba99233f23497f11b8160d3053208c87e8
Maxim Khalyapin <nekokat89@gmail.com>
2024-04-08 22:42:43 +0300

    update

--- Workspace/Modifier/Modifier.cs
+++ Workspace/Modifier/Modifier.cs
@@ -8,13 +8,13 @@
 
         }
 
-        public BlankID Id {get; set;}
-        public BlankType Type {get; set;}
-        public string Name {get; set;}
+        public BlankID? Id {get; set;}
+        public BlankType? Type {get; set;}
+        public string? Name {get; set;}
         public int? Cost {get; set;}
         public bool? Disabled {get; set;}
-        public string Notes {get; set;}
-        public string Affects {get; set;}
-        public string CostType {get; set;}
+        public string? Notes {get; set;}
+        public string? Affects {get; set;}
+        public string? CostType {get; set;}
     }
 }
