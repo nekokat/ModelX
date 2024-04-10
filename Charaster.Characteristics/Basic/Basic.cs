2eca710b2fcb6561022d4b472a68ab217eb4f782
Maxim Khalyapin <nekokat89@gmail.com>
2024-04-10 21:52:35 +0300

    add LoadCostTest

--- Charaster.Characteristics/Basic/Basic.cs
+++ Charaster.Characteristics/Basic/Basic.cs
@@ -1,4 +1,7 @@
-﻿using Types;
+﻿using System.Diagnostics;
+using Types;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Characteristics{
 
@@ -11,7 +14,50 @@ namespace Characteristics{
 
         public AttributeType Type { get; set; }
 
-        public int Cost { get; set; }
+        public int Cost {
+            get {
+                return CType switch
+                {
+                    CostType.Points => CostSetPoints(Type),
+                    CostType.Percentage => CostSetPercentage(Type),
+                    CostType.Multiplier => CostSetMultiplier(Type),
+                    _ => throw new Exception()
+                };
+            }
+        }
+
+        private int CostSetMultiplier(AttributeType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int CostSetPercentage(AttributeType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int CostSetPoints(AttributeType type)
+        {
+            /*
+            return type switch
+            {
+                AttributeType.Dexterity => 20,
+                AttributeType.Health => 10,
+                AttributeType.Intelligence => 20,
+                AttributeType.Strength => 10,
+                _ => throw new Exception()
+            };
+            */
+
+            Dictionary<AttributeType, int>? dictionary;
+            using (StreamReader r = new StreamReader("file.json"))
+            {
+                string json = r.ReadToEnd();
+                dictionary = JsonConvert.DeserializeObject<Dictionary<AttributeType, int>>(json);
+                
+            }
+            return dictionary?[type] ?? 0;
+        }
         
         public CostType CType{ get; set; }
     }
---
+++ Tests/LoadCostTests.cs
@@ -0,0 +1,21 @@
+namespace Tests
+{
+    public class CostTest
+    {
+        [SetUp]
+        public void Setup(){}
+
+        [Test]
+        public void LoadCostFromJson()
+        {
+            string file = @"{    
+                'Dexterity': 20,
+                'Health': 10,
+                'Intelligence': 20,
+                'Strength': 10
+            }
+            ";
+            
+        }
+    }
+}
